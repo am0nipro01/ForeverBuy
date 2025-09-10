0ef5047e9d7c6d583abd31eb8ec09feed596f99b
am0nipro01 <amonipro01@gmail.com>
2025-09-10 18:13:04 +0200

    feat(navbar): add Navbar section, styling and responsive with hamb menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,14 @@
 import { Routes, Route } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Cart from "./pages/Cart";
+import Collection from "./pages/Collection";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import Orders from "./pages/Orders";
+import PlaceOrder from "./pages/PlaceOrder";
+import Product from "./pages/Product";
 
 const App = () => {
   // state, data
@@ -8,7 +18,8 @@ const App = () => {
   // render
   return (
     <>
-      <div className=".px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+        <Navbar />
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path="/about" element={<About/>} />
